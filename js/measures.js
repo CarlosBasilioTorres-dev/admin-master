45ada46938e226add1b651ac543926b4b3ac3f87
DesarrollosSTC <Desarrollo.SCMx@savethechildren.org>
2022-02-22 21:53:21 -0600

    Corrigiendo JS y maquetado HTML

--- js/measures.js
+++ js/measures.js
@@ -5,15 +5,46 @@ class Measures{
         console.warn("Welcome to adminMaster, this class is an example to learn how to use this library, all errors will appear in spanish and english to understand it better");
 
         this.properties = properties;
-        this.cards = properties.cards;
-        this.values = properties.values;
-        this.dark = properties.dark;
-
         this.init()
     }
 
+    init(){        
+        this.setContainer();
+        this.setCardElements();
+
+        setInterval(this.startAnimation,10)
+        this.setDarkMode();
+    }
+
+    setContainer(){
+        this.cardsContainer = document.createElement('div');
+        this.cardsContainer.classList.add('cards-container');
+        this.cardsContainer.classList.add('col-2');
+
+        document.querySelector(this.properties.container).appendChild(this.cardsContainer);
+    }
+
     setCardElements(){
-        this.values.forEach((value,index) => {
+        switch(this.properties.type){
+            case 'percent-circle-one':
+                this.setPercentCircleOne();
+                break;
+        }
+    }
+ 
+    startAnimation(){
+        var self = this;
+        document.querySelectorAll(".card-container").forEach((card,index) => {
+            var element = card.querySelector(".progress-bar").style.height.replace("%","");
+            if(card.getAttribute('data-value').localeCompare(element) != 0){
+                card.querySelector(".progress-bar").style.height = (Number(element) + 1) + "%";
+                card.querySelector(".card-number").textContent = (Number(element) + 1) + "%";
+            }
+        })
+    }
+
+    setPercentCircleOne(){
+        this.properties.values.forEach((value,index) => {
             var element = document.createElement('div');
             element.classList.add("card-container")
             element.setAttribute('data-value',value.value);
@@ -47,48 +78,20 @@ class Measures{
             body.appendChild(progress_container);
             element.appendChild(body);
             element.appendChild(card_concept);
-            this.container.appendChild(element);
-        })
-    }
-
-    setContainer(){
-        this.container = document.createElement('div');
-        this.container.classList.add('cards-container');
-        this.container.classList.add('col-2');
-
-        document.querySelector(this.properties.container).appendChild(this.container);
-    }
-
-    startAnimation(){
-        var self = this;
-        document.querySelectorAll(".card-container").forEach((card,index) => {
-            var element = card.querySelector(".progress-bar").style.height.replace("%","");
-            if(card.getAttribute('data-value').localeCompare(element) != 0){
-                card.querySelector(".progress-bar").style.height = (Number(element) + 1) + "%";
-                card.querySelector(".card-number").textContent = (Number(element) + 1) + "%";
-            }
+            this.cardsContainer.appendChild(element);
         })
     }
 
     setDarkMode(){
-    
+
+        if(this.properties.dark === undefined){
+            this.properties.dark = false;
+        }
+
         if(this.dark){
             document.querySelector(this.properties.container).classList.add("dark-mode");
         }else{
             document.querySelector(this.properties.container).classList.remove("dark-mode");
         }
     }
-
-    init(){
-        if(this.dark === undefined){
-            this.dark = false;
-        }
-        
-        this.setContainer();
-        this.setCardElements();
-        setInterval(this.startAnimation,10)
-        
-        this.setDarkMode();
-    }
-
 }
